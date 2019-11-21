53613fc37c7e96a76b0e592c94a6a87a0286b7ae
Isabel Brandin <isabel.brandin@skola.botkyrka.se>
2019-11-21 11:06:11 +0100

    Har lagt till en animation

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 pygame.init()
 
@@ -10,27 +11,45 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+GREY = (185, 185, 185)
+LIGHT_PINK = (247, 194, 151)
 
 done = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
- 
+
+rect_x = 50
+rect_y = 50
+rect_change_x = 5
+rect_change_y = 5
+
 # -------- Main Program Loop -----------
 while not done:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
  
-    screen.fill(WHITE)
+    screen.fill(BLACK)
     # --- Drawing code should go here
+
+    pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
+    pygame.draw.rect(screen, LIGHT_PINK, [rect_x + 10, rect_y + 10, 30, 30])
+    
+    rect_x += rect_change_x
+    rect_y += rect_change_y
+
+    if rect_x > 650 or rect_x < 0:
+        rect_change_x = rect_change_x * -1
+    if rect_y > 450 or rect_y < 0:
+        rect_change_y = rect_change_y * -1
+
     for x_offset in range(30, 300, 30):
         pygame.draw.line(screen, GREEN, [x_offset, 100], [x_offset-10, 90], 2)
         pygame.draw.line(screen, RED, [x_offset, 90], [x_offset-10, 100], 2)
         pygame.draw.line(screen, BLUE, [x_offset, 300], [x_offset-10, 290], 2)
-        pygame.draw.line(screen, BLACK, [x_offset, 290], [x_offset-10, 300], 2)
+        pygame.draw.line(screen, GREY, [x_offset, 290], [x_offset-10, 300], 2)
    
-    pygame.draw.rect(screen, BLUE, [30, 150, 230, 90], 2)
    
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
