1e9687edb4cfe790d814fba086efcbf20122e39f
Isabel Brandin <isabel.brandin@skola.botkyrka.se>
2019-11-25 15:56:46 +0100

    tog bort kvadraten och la dit något gräsliknande

--- test.py
+++ test.py
@@ -27,31 +27,28 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-rect_x = 50
-rect_y = 50
-rect_change_x = 5
-rect_change_y = 5
 x_speed = 0
 y_speed = 0
 x_coord = 300
-y_coord = 150
+y_coord = 265
 
 # -------- Main Program Loop -----------
 while not done:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
+       
         # --- Drawing code should go here
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x_speed = -3
-            elif event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT:
                 x_speed = 3
-            elif event.key == pygame.K_UP:
+            if event.key == pygame.K_UP:
                 y_speed = -3
-            elif event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN:
                 y_speed = 3
-        
+            
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_speed = 0
@@ -65,16 +62,7 @@ while not done:
     
     draw_stick_figure(screen, x_coord, y_coord)
 
-    pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
-    pygame.draw.rect(screen, LIGHT_ORANGE, [rect_x + 10, rect_y + 10, 30, 30])
-    
-    rect_x += rect_change_x
-    rect_y += rect_change_y
-
-    if rect_x > 650 or rect_x < 0:
-        rect_change_x = rect_change_x * -1
-    if rect_y > 450 or rect_y < 0:
-        rect_change_y = rect_change_y * -1
+    pygame.draw.rect(screen, GREEN, [0, 400, 700, 400], 0)
 
    
    
